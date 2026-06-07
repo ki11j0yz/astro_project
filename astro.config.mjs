c40635269a2c46823fe28e0d4a5c6a796d812536
James <jkprice02@gmail.com>
2026-06-07 15:43:09 -0500

    Secondary commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://ki11j0yz.github.io', // Your GitHub Pages URL
+    base: '/astro_project',             // Your repository name (must have leading slash)
+});
